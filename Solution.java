829cdb708bca3085ea262585db6e167df3f95465
kifil <wilsondr16@gmail.com>
2017-03-20 22:45:09 -0500

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+        
+        //priorityqueue == heap orderd binary tree
+        //use a min quueu and a max queue
+        //max queue is left half, min is right half
+        
+        //if queues are equal size
+            //add to either queue and resort 
+        
+        //if queues are not equal
+            //add ot smaller and resort
+            //if add ot max and maxQueue head > minQueue head, swap heads and resort
+        //invert too
+            //now invariat should be left tree always has smaller half of numbers and right tree has larger half
+            
+        
+        
+           
+    }
+}
